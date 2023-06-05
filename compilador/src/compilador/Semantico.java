c0ddf5a8ff25448d1bfc65970c8566bdeff1a7d1
GlhrmeSoares <64655650+GlhrmeSoares@users.noreply.github.com>
2023-06-05 15:33:28 -0300

    Implementação e Correção
    Implementei as ações 2, 8,11, 13, 15 e 16. Fiz também algumas correções como trocar o peek por pop na pilha para retirar o valor da pilha onde nao o estava fazendo.

--- compilador/src/compilador/Semantico.java
+++ compilador/src/compilador/Semantico.java
@@ -1,11 +1,10 @@
 package compilador;
-
+//  19 15
 import java.util.Stack;
 
 public class Semantico implements Constants {
 	
-	private String operador = "";
-	private String codigo;
+	private StringBuilder codigo;
 	private Stack<String> pilhaTipos;
 	String tipoFloat = "float64";
 	String tipoInt = "int64";
@@ -16,8 +15,8 @@ public class Semantico implements Constants {
 	}
     
     public void doAcaoSemantica1() {
-    	String tipo1 = pilhaTipos.peek();
-    	String tipo2 = pilhaTipos.peek();
+    	String tipo1 = pilhaTipos.pop();// não desempilhou
+    	String tipo2 = pilhaTipos.pop();
 
 		// transformado em atributo de classe
     	//String tipoFloat = "float64";
@@ -28,23 +27,48 @@ public class Semantico implements Constants {
     	} else {
     		pilhaTipos.push(tipoInt);
     	}
-    	// TODO codigo.adiciona(add);
+    	codigo.append("add");
 
     }
     
     public void doAcaoSemantica2() {
     	
+    	String tipo1 = pilhaTipos.pop();
+    	String tipo2 = pilhaTipos.pop();
+    	
+    	if(tipo1.equals(tipoFloat) || tipo2.equals(tipoFloat)) {
+    		pilhaTipos.push(tipoFloat);
+    	}else {
+    		pilhaTipos.push(tipoInt);
+    	}
+    	codigo.append("\nsub\n");
     }
 
 	public void doAcaoSemantica5(Token token) {
 		pilhaTipos.push(tipoInt);
-		codigo += token.getLexeme();
-		codigo += ".0";
+		codigo.append(token.getLexeme());
+		codigo.append(".0");
+	}
+	
+	public void doAcaoSemantica8() {
+		codigo.append("\nldc.i8 -1\n");
+		codigo.append("\nconv.r8\n");
+		codigo.append("\nmul\n");
 	}
 
+	public void doAcaoSemantica11() {
+		pilhaTipos.push(tipoBoolean);
+		codigo.append("\nldc.i4.1\n");
+	}
+	
 	public void doAcaoSemantica12() {
 		pilhaTipos.push(tipoBoolean);
-		codigo += "false";
+		codigo.append("false");
+	}
+	
+	public void doAcaoSemantica13() {
+		codigo.append("ldc.i4.1");
+		codigo.append("xor");
 	}
 
 	public void doAcaoSemantica14() {
@@ -53,15 +77,28 @@ public class Semantico implements Constants {
 			// como converter para inteiro?
 		}
 
-		codigo += "(" + tipo + ")";
+		codigo.append("(" + tipo + ")");
 		System.out.println("(" + tipo + ")");
 
 	}
+	
+	public void doAcaoSemantica15() {
+		codigo.append((".assembly extern mscorlib{} \n.assembly _codigo_objeto{} \n.module _codigo_objeto.exe  "
+			   +"\n.class public _UNICA{ \n.method static public void _principal(){ \n.entrypoint"));
+	}
+	
+	public void doAcaoSemantica16() {
+		// TODO codigo.adiciona(ret }});
+	}
 
+	public void doAcaoSemantica19() {
+		
+	}
+	
 	public void doAcaoSemantica20(Token token) { // TODO
 		pilhaTipos.push(tipoInt);
-		codigo += token.getLexeme();
-		codigo += ".0";
+		codigo.append(token.getLexeme());
+		codigo.append(".0");
 	}
 
 	public void doAcaoSemantica10() {
